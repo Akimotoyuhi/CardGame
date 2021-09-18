5b6bb41ee57c17bffdc48f50f8f10c8b4b21d1d0
AkimotoYuhi <chiisanaaki@gmail.com>
2021-09-19 01:39:10 +0900

    ドラッグ＆ドロップでドロップ先のオブジェクトを取れるようにした

---
+++ Assets/Scripts/DragObj.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragObj : MonoBehaviour,IDragHandler,IBeginDragHandler,IEndDragHandler
+{
+    //ドラッグしたいオブジェクトに付ける
+    private Vector2 m_defPos;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        m_defPos = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = m_defPos;
+    }
+}
---
+++ Assets/Scripts/DropObj.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropObj : MonoBehaviour,IDropHandler
+{
+    //ドロップしたいオブジェクトに付ける
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log($"{gameObject.name}に{eventData.pointerDrag.name}がドロップされました");
+    }
+}
