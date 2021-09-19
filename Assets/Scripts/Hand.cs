94821b78e641d469958094c6a4a65cda6aca4152
AkimotoYuhi <chiisanaaki@gmail.com>
2021-09-19 20:26:53 +0900

    手札をHorizontalLayoutGroupで整列させた

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
