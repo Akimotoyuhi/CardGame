9726f9e2d5aba6a57f7fb9f811675ec3a2a7ff39
AkimotoYuhi <chiisanaaki@gmail.com>
2022-02-12 19:57:11 +0900

    敵の行動予定の画像にマウスポインタを当てると詳細が表示される機能

--- Assets/Scripts/Enemy/PlanController.cs
+++ Assets/Scripts/Enemy/PlanController.cs
@@ -1,48 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 /// <summary>
 /// Enemyの行動予定を受け持つクラス<br/>EnemyBaseクラスから呼ばれる事を前提としている
 /// </summary>
-public class PlanController : MonoBehaviour
+public class PlanController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField, Tooltip("行動予定表示用の画像 表示順は\nAttack\nBlock\nBuff\nDebuff\nUnknown\nの順")]
-    Sprite[] m_viewPlanSprite;
-    [SerializeField] Text m_text;
-    private Image m_image;
+    [SerializeField] Text m_viewText;
+    [SerializeField] Image m_image;
+    [SerializeField] List<PlanSetting> m_planController;
+    private string m_text = default;
+    [System.Serializable]
+    public class PlanSetting
+    {
+        [Header("行動予定表示用の画像 表示順はAttack, Block, Buff, Debuff, Unknownの順")]
+        [SerializeField] Sprite m_sprite;
+        [SerializeField, TextArea] string m_text;
+        [SerializeField] Color m_color;
+        public string Text => m_text;
+        public void SetImage(Image image)
+        {
+            image.sprite = m_sprite;
+            image.color = m_color;
+        }
+    }
 
     public void SetImage(ActionPlan actionPlan, int textValue = 0)
     {
-        m_image = GetComponent<Image>();
-        m_text.text = "";
+        m_viewText.text = "";
+        m_planController[(int)actionPlan].SetImage(m_image);
+        m_text = m_planController[(int)actionPlan].Text;
         switch (actionPlan)
         {
             case ActionPlan.Attack:
-                m_image.sprite = m_viewPlanSprite[0];
-                m_image.color = Color.red;
-                m_text.text = textValue.ToString();
-                break;
-            case ActionPlan.Block:
-                m_image.sprite = m_viewPlanSprite[1];
-                m_image.color = Color.blue;
-                break;
-            case ActionPlan.Buff:
-                m_image.sprite = m_viewPlanSprite[2];
-                m_image.color = Color.blue;
-                break;
-            case ActionPlan.Debuff:
-                m_image.sprite = m_viewPlanSprite[3];
-                m_image.color = Color.green;
-                break;
-            case ActionPlan.Unknown:
-                m_image.sprite = m_viewPlanSprite[4];
-                m_image.color = Color.white;
+                m_viewText.text = textValue.ToString();
                 break;
             default:
-                Debug.LogError("変な値渡すな");
                 break;
         }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        EffectManager.Instance.SetBattleUIText(m_text, Color.black);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        EffectManager.Instance.RemoveBattleUIText();
     }
 }
