3204acb15842fae2302ad303fa85b75dd8bd7590
AkimotoYuhi <chiisanaaki@gmail.com>
2022-02-24 14:40:36 +0900

    コスト説明文追記

---
+++ Assets/Scripts/UI/CostView.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CostView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField, TextArea] string m_text;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        EffectManager.Instance.SetBattleUIText(m_text, Color.black);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        EffectManager.Instance.RemoveBattleUIText();
+    }
+}
