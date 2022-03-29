189c9cdfd1d2bcd6cc6aeb45783078dc3cf43927
AkimotoYuhi <chiisanaaki@gmail.com>
2022-03-29 17:11:43 +0900

    カードデータに使用条件を追加できるようにした

--- Assets/Scripts/Card/CardData.cs
+++ Assets/Scripts/Card/CardData.cs
@@ -59,6 +59,180 @@ public class CardData : ScriptableObject
         return list[r].GetCardInfo(upgradeNum);
     }
 }
+/// <summary>カードのデータベース<br/>カードの効果の部分</summary>
+[Serializable]
+public class CardDataBase
+{
+    public string m_rabel;
+    [SerializeField] CardInfomationData m_cardInfo;
+    [SerializeField] CardInfomationData m_cardUpgrade;
+    public void Setup(CardID id)
+    {
+        m_cardInfo.CardId = id;
+        m_cardUpgrade.CardId = id;
+    }
+    /// <summary>強化前後のカード情報<br/>引数は複数回のカード強化を想定してintにしている</summary>
+    /// <param name="upgradeNum">強化前後。0が前、1が後</param>
+    /// <returns>カード情報</returns>
+    public CardInfomationData GetCardInfo(int upgradeNum)
+    {
+        switch (upgradeNum)
+        {
+            case 0:
+                return m_cardInfo;
+            case 1:
+                return m_cardUpgrade;
+            default:
+                Debug.LogError("これ以上設定された強化データが存在しない");
+                return null;
+        }
+    }
+}
+/// <summary>カードの使用条件/効果の発動条件</summary>
+[Serializable]
+public class CardConditional
+{
+    /// <summary>条件を適応しない</summary>
+    [SerializeField] bool m_doesNotAdapt;
+    [SerializeField] CardConditionalEvaluationParam m_evaluationParam;
+    [SerializeField] CardConditionalEvaluationType m_evaluationType;
+    [SerializeField] CardUsedConditional m_cardConditional;
+    [SerializeField] int m_num;
+    /// <summary>
+    /// 条件の評価
+    /// </summary>
+    /// <param name="evaluationParam">評価したいパラメーターの種類</param>
+    /// <param name="evaluationType">評価したい対象</param>
+    /// <param name="num">評価したい値</param>
+    /// <returns></returns>
+    public bool Evaluation(CardConditionalEvaluationParam evaluationParam, CardConditionalEvaluationType evaluationType, int num)
+    {
+        if (m_doesNotAdapt)
+            return true;
+        if (evaluationParam != m_evaluationParam || evaluationType != m_evaluationType) return false;
+        switch (m_cardConditional)
+        {
+            case CardUsedConditional.High:
+                if (m_num <= num)
+                    return true;
+                break;
+            case CardUsedConditional.Low:
+                if (m_num >= num)
+                    return true;
+                break;
+            case CardUsedConditional.Even:
+                if (num % 2 == 0)
+                    return true;
+                break;
+            case CardUsedConditional.Odd:
+                if (num % 2 != 0)
+                    return true;
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+}
+/// <summary>カードデータの使用時の効果の部分</summary>
+[Serializable]
+public class CardInfomationData
+{
+    [SerializeField] string m_name;
+    [SerializeField] string m_cost;
+    [SerializeField] Sprite m_image;
+    [TextArea(0, 5), Tooltip("変数に差し替えたい部分は{leg0}(数値の部分は配列番号)のように記述する事")]
+    [SerializeField] string m_tooltip;
+    [SerializeField] Rarity m_rarity;
+    [SerializeReference, SubclassSelector] List<ICommand> m_commands;
+    [SerializeField] CardConditional m_cardConditional;
+    [SerializeField] UseType m_cardType;
+    [SerializeField] bool m_isDiscarding = false;
+    [SerializeField] bool m_ethereal = false;
+    /// <summary>カードの名前</summary>
+    public string Name => m_name;
+    /// <summary>コスト</summary>
+    public string Cost => m_cost; //コストは数字じゃない可能性があるのでとりあえずstringにしとく
+    /// <summary>画像</summary>
+    public Sprite Sprite => m_image;
+    /// <summary>効果の説明文</summary>
+    public string Tooltip => m_tooltip;
+    /// <summary>カードID</summary>
+    public CardID CardId { get; set; }
+    /// <summary>レアリティ</summary>
+    public Rarity Rarity => m_rarity;
+    /// <summary>
+    /// カード使用時の効果<br/>{ 効果の種類(CommandParam), 発動対象(UseType), 効果(int) }
+    /// </summary>
+    public List<int[]> Command
+    {
+        get
+        {
+            List<int[]> ret = new List<int[]>();
+            foreach (var c in m_commands)
+                ret.Add(c.Execute());
+            return ret;
+        }
+    }
+    public CardConditional CardConditional => m_cardConditional;
+    /// <summary>使用する標的</summary>
+    public UseType UseType => m_cardType;
+    /// <summary>廃棄カード</summary>
+    public bool IsDiscarding => m_isDiscarding;
+    public bool Ethereal => m_ethereal;
+}
+
+public interface ICommand
+{
+    /// <summary>カードを使用した時の効果</summary>
+    /// <returns>{ 効果の種類(CommandParam), 発動対象(UseType), 効果(int) }</returns>
+    int[] Execute();
+}
+[Serializable]
+public class CardAttackCommand : ICommand
+{
+    [SerializeField, Tooltip("何ダメージを与えるか")] int m_power;
+    [SerializeField, Tooltip("付与対象")] UseType m_useType;
+    public int[] Execute() => new int[] { (int)CommandParam.Attack, (int)m_useType, m_power };
+}
+[Serializable]
+public class CardBlockCommnad : ICommand
+{
+    [SerializeField, Tooltip("何ブロックを得るか")] int m_block;
+    [SerializeField, Tooltip("付与対象")] UseType m_useType;
+    public int[] Execute() => new int[] { (int)CommandParam.Block, (int)m_useType, m_block };
+}
+[Serializable]
+public class CardConditionCommand : ICommand
+{
+    [SerializeField, Tooltip("付与するバフデバフの設定")] ConditionSelection m_condition;
+    [SerializeField, Tooltip("付与対象")] UseType m_useType;
+    public int[] Execute() => new int[] { (int)CommandParam.Conditon, (int)m_useType, (int)m_condition.GetCondition.GetConditionID(), m_condition.GetCondition.Turn };
+}
+[Serializable]
+public class AddCardCommand : ICommand
+{
+    [SerializeField, Tooltip("カードの追加枚数")] int m_addNum;
+    [SerializeField, Tooltip("追加するカードのID")] CardID m_cardID;
+    [SerializeField, Tooltip("カード追加先")] CardAddDestination m_cardAddDestination;
+    [SerializeField, Tooltip("追加するカードを強化済みにする")] bool m_isUpgrade;
+    public int[] Execute()
+    {
+        int i = m_isUpgrade ? 1 : 0;
+        return new int[] { (int)CommandParam.AddCard, (int)UseType.System, (int)m_cardID, m_addNum, (int)m_cardAddDestination, i };
+    }
+}
+
+public class DrawCardCommand : ICommand
+{
+    [SerializeField, Tooltip("カードのドロー(or捨てる)枚数")] int m_drawNum;
+    [SerializeField, Tooltip("trueならドローする、falseなら捨てる")] bool m_isDraw;
+    public int[] Execute()
+    {
+        int i = m_isDraw ? 1 : 0;
+        return new int[] { (int)CommandParam.DrawCard, (int)UseType.System, i, m_drawNum };
+    }
+}
 #region Enums
 /// <summary>カードのレア度</summary>
 public enum CardID
@@ -137,130 +311,30 @@ public enum CardAddDestination
     ToHand,
     ToDiscard,
 }
+/// <summary>カード使用条件にて評価したい対象</summary>
+public enum CardConditionalEvaluationType
+{
+    Player,
+    Enemy,
+    Hand,
+    Discard,
+    Deck,
+}
+/// <summary>カード使用条件にて評価したいパラメーター</summary>
+public enum CardConditionalEvaluationParam
+{
+    Power,
+    Block,
+    Life,
+    BuffDebuff,
+    Other,
+}
+/// <summary>カードの使用条件</summary>
+public enum CardUsedConditional
+{
+    High,
+    Low,
+    Even,//偶数
+    Odd,//奇数
+}
 #endregion
-/// <summary>カードのデータベース<br/>カードの効果の部分</summary>
-[Serializable]
-public class CardDataBase
-{
-    public string m_rabel;
-    [SerializeField] CardInfomationData m_cardInfo;
-    [SerializeField] CardInfomationData m_cardUpgrade;
-    public void Setup(CardID id)
-    {
-        m_cardInfo.CardId = id;
-        m_cardUpgrade.CardId = id;
-    }
-    /// <summary>強化前後のカード情報<br/>引数は複数回のカード強化を想定してintにしている</summary>
-    /// <param name="upgradeNum">強化前後。0が前、1が後</param>
-    /// <returns>カード情報</returns>
-    public CardInfomationData GetCardInfo(int upgradeNum)
-    {
-        switch (upgradeNum)
-        {
-            case 0:
-                return m_cardInfo;
-            case 1:
-                return m_cardUpgrade;
-            default:
-                Debug.LogError("これ以上設定された強化データが存在しない");
-                return null;
-        }
-    }
-}
-/// <summary>カードデータの使用時の効果の部分</summary>
-[Serializable]
-public class CardInfomationData
-{
-    [SerializeField] string m_name;
-    [SerializeField] string m_cost;
-    [SerializeField] Sprite m_image;
-    [TextArea(0, 5), Tooltip("変数に差し替えたい部分は{leg0}(数値の部分は配列番号)のように記述する事")]
-    [SerializeField] string m_tooltip;
-    [SerializeField] Rarity m_rarity;
-    [SerializeReference, SubclassSelector] List<ICommand> m_commands;
-    [SerializeField] UseType m_cardType = new UseType();
-    [SerializeField] bool m_isDiscarding = false;
-    [SerializeField] bool m_ethereal = false;
-    /// <summary>カードの名前</summary>
-    public string Name => m_name;
-    /// <summary>コスト</summary>
-    public string Cost => m_cost; //コストは数字じゃない可能性があるのでとりあえずstringにしとく
-    /// <summary>画像</summary>
-    public Sprite Sprite => m_image;
-    /// <summary>効果の説明文</summary>
-    public string Tooltip => m_tooltip;
-    /// <summary>カードID</summary>
-    public CardID CardId { get; set; }
-    /// <summary>レアリティ</summary>
-    public Rarity Rarity => m_rarity;
-    /// <summary>
-    /// カード使用時の効果<br/>{ 効果の種類(CommandParam), 発動対象(UseType), 効果(int) }
-    /// </summary>
-    public List<int[]> Command
-    {
-        get
-        {
-            List<int[]> ret = new List<int[]>();
-            foreach (var c in m_commands)
-                ret.Add(c.Execute());
-            return ret;
-        }
-    }
-    /// <summary>使用する標的</summary>
-    public UseType UseType => m_cardType;
-    /// <summary>廃棄カード</summary>
-    public bool IsDiscarding => m_isDiscarding;
-    public bool Ethereal => m_ethereal;
-}
-
-public interface ICommand
-{
-    /// <summary>カードを使用した時の効果</summary>
-    /// <returns>{ 効果の種類(CommandParam), 発動対象(UseType), 効果(int) }</returns>
-    int[] Execute();
-}
-[Serializable]
-public class CardAttackCommand : ICommand
-{
-    [SerializeField, Tooltip("何ダメージを与えるか")] int m_power;
-    [SerializeField, Tooltip("付与対象")] UseType m_useType;
-    public int[] Execute() => new int[] { (int)CommandParam.Attack, (int)m_useType, m_power };
-}
-[Serializable]
-public class CardBlockCommnad : ICommand
-{
-    [SerializeField, Tooltip("何ブロックを得るか")] int m_block;
-    [SerializeField, Tooltip("付与対象")] UseType m_useType;
-    public int[] Execute() => new int[] { (int)CommandParam.Block, (int)m_useType, m_block };
-}
-[Serializable]
-public class CardConditionCommand : ICommand
-{
-    [SerializeField, Tooltip("付与するバフデバフの設定")] ConditionSelection m_condition;
-    [SerializeField, Tooltip("付与対象")] UseType m_useType;
-    public int[] Execute() => new int[] { (int)CommandParam.Conditon, (int)m_useType, (int)m_condition.GetCondition.GetConditionID(), m_condition.GetCondition.Turn };
-}
-[Serializable]
-public class AddCardCommand : ICommand
-{
-    [SerializeField, Tooltip("カードの追加枚数")] int m_addNum;
-    [SerializeField, Tooltip("追加するカードのID")] CardID m_cardID;
-    [SerializeField, Tooltip("カード追加先")] CardAddDestination m_cardAddDestination;
-    [SerializeField, Tooltip("追加するカードを強化済みにする")] bool m_isUpgrade;
-    public int[] Execute()
-    {
-        int i = m_isUpgrade ? 1 : 0;
-        return new int[] { (int)CommandParam.AddCard, (int)UseType.System, (int)m_cardID, m_addNum, (int)m_cardAddDestination, i };
-    }
-}
-
-public class DrawCardCommand : ICommand
-{
-    [SerializeField, Tooltip("カードのドロー(or捨てる)枚数")] int m_drawNum;
-    [SerializeField, Tooltip("trueならドローする、falseなら捨てる")] bool m_isDraw;
-    public int[] Execute()
-    {
-        int i = m_isDraw ? 1 : 0;
-        return new int[] { (int)CommandParam.DrawCard, (int)UseType.System, i, m_drawNum };
-    }
-}
